f6b35b06e1eaf9f1d294d9b7f5b38765e9ae1259
Пупкин Василий Иванович <example@example.com>
2024-09-21 03:13:41 +0500

    added test

---
+++ Tyuiu.BarbashinAV.Sprint2.Task2.V10.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+using Tyuiu.BarbashinAV.Sprint2.Task2.V10.Lib;
+
+namespace Tyuiu.BarbashinAV.Sprint2.Task2.V10.Test;
+
+[TestClass]
+public class DataServiceTest
+{
+    [TestMethod]
+    public void ValidExpression()
+    {
+        Assert.AreEqual(true, new DataService().CheckDotInShadedArea(6, 8));
+    }
+}
